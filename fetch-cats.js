f8f22f2f7b747ebb2a109fdcc0b064568aa810b4
zntb <zenetibi@yahoo.com>
2023-11-15 18:28:06 +0200

    Add wikipedia - total page views to Cat API

--- fetch-cats.js
+++ fetch-cats.js
@@ -34,11 +34,42 @@ function handleButtonClick() {
 
   const outputSpan = document.getElementById('output');
 
+  const cat = catArray[index];
+
+  const indexOfLastSlash = cat.wikipedia_url.lastIndexOf('/');
+
+  let wikiSearchTerm = cat.wikipedia_url.slice(indexOfLastSlash + 1);
+
   let output = `<h2>${catArray[index].name}</h2>`;
-  output += `<img src="${catArray[index].image.url}"><br>`;
-  output += `<h3>Description</h3><p>${catArray[index].description}</p>`;
+  output += `<img src="${cat.image.url}"><br>`;
+  output += `<h3>Description</h3><p>${cat.description}</p>`;
+  output += `<br>Click to learn about <a href="${cat.wikipedia_url}">${cat.name}</a>`;
 
   outputSpan.innerHTML = output;
+
+  showTotalPageviews(wikiSearchTerm, outputSpan);
+}
+
+function showTotalPageviews(searchTerm, outputSpan) {
+  let totalPageViews = 0;
+
+  let searchDate = '2023-10-01/2023-11-01';
+  fetch(
+    `https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/${searchTerm}/daily/${searchDate.replace(
+      /-/g,
+      ''
+    )}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const dataArray = data.items;
+
+      dataArray.forEach((dayData) => {
+        totalPageViews += parseInt(dayData.views);
+      });
+
+      outputSpan.innerHTML += `<br>TotalPageViews for ${searchDate}: <strong>${totalPageViews}</strong><br>`;
+    });
 }
 
 window.onload = start;
